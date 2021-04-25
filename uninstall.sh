9bedf1bd5b2cc438a6374cacce3ab7d5a4aabc57
SMSourov <81343486+SMSourov@users.noreply.github.com>
2021-04-25 07:15:12 +0600

    The uninstaller.
    I've created uninstaller according to the latest version of Oracle JDK(which is 16.0.1).

---
+++ uninstall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Removing Oracle JDK."
+# This will remove the JDK package and the compiler.
+sudo apt purge --remove jdk-16.0.1 -y
+
+echo "Removing JDK environment."
+# This will remove the Java Runtime Environment.
+sudo rm /etc/profile.d/jdk-16.0.1.sh
+
+# This wil remove java, javac and jar from update-alternatives.
+sudo update-alternatives --remove java /usr/lib/jvm/jdk-16.0.1/bin/java
+sudo update-alternatives --remove jar /usr/lib/jvm/jdk-16.0.1/bin/jar
+sudo update-alternatives --remove javac /usr/lib/jvm/jdk-16.0.1/bin/javac
+
+echo "Oracle JDK removed."
+echo "However, you may need to restart your system."
