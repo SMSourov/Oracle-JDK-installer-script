44ea3cd4bd2d452344c67766910a7c2a291f1bd6
SMSourov <81343486+SMSourov@users.noreply.github.com>
2021-09-18 23:27:10 +0600

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,16 +2,16 @@
 
 echo "Removing Oracle JDK."
 # This will remove the JDK package and the compiler.
-sudo apt purge --remove jdk-16.0.2 -y
+sudo apt purge --remove jdk-17 -y
 
 echo "Removing JDK environment."
 # This will remove the Java Runtime Environment.
-sudo rm /etc/profile.d/jdk-16.0.2.sh
+sudo rm /etc/profile.d/jdk-17.sh
 
 # This wil remove java, javac and jar from update-alternatives.
-sudo update-alternatives --remove java /usr/lib/jvm/jdk-16.0.2/bin/java
-sudo update-alternatives --remove jar /usr/lib/jvm/jdk-16.0.2/bin/jar
-sudo update-alternatives --remove javac /usr/lib/jvm/jdk-16.0.2/bin/javac
+sudo update-alternatives --remove java /usr/lib/jvm/jdk-17/bin/java
+sudo update-alternatives --remove jar /usr/lib/jvm/jdk-17/bin/jar
+sudo update-alternatives --remove javac /usr/lib/jvm/jdk-17/bin/javac
 
 echo "Oracle JDK removed."
 echo "However, you may need to restart your system."
